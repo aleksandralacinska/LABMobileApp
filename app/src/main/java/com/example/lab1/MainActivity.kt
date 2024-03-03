5b4dea384720b4ac5996f35346082eed8ff129da
Aleksandra <aleksandra.lacinska@gmail.com>
2024-03-03 12:38:09 +0100

    clicker

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.lab1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var clickCount = 0
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        textView = findViewById(R.id.textView)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            clickCount++
+            textView.text = "You have clicked $clickCount ${if (clickCount == 1) "time" else "times"}"
         }
     }
 }
